8bf98c59dbf509803b87a7f9c6aae8a0483b3654
ITschoolKDF <p7oEkmy^>
2020-06-23 17:33:22 +0300

    Update to Android Studio 4

---
+++ app/src/main/java/ru/samsung/itschool/book/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.samsung.itschool.book.myapplication;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
